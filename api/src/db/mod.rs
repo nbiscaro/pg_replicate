c88745aec6bc33cbe81ee20143cdb19e428a1914
Nolan Biscaro <biscaronolan@gmail.com>
2025-03-26 12:06:25 -0400

    remove api

--- api/src/db/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod images;
-pub mod pipelines;
-pub mod publications;
-pub mod replicators;
-pub mod sinks;
-pub mod sources;
-pub mod tables;
-pub mod tenants;
--- api/tests/api/main.rs
+++
@@ -1,8 +0,0 @@
-mod database;
-mod health_check;
-mod images;
-mod pipelines;
-mod sinks;
-mod sources;
-mod tenants;
-mod test_app;
